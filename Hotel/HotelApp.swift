9dfeb53154417da195724f649c6e34333eb10f63
AlexAlexBoga <AlexAlexBoga@users.noreply.github.com>
2024-10-01 17:40:03 +0300

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Александр Богачев on 1.10.24.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
